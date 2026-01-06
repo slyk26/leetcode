bb3f9fea22f5ed6325ce146b2f6ef02ced1a2c26
slyk26 <102811034+slyk26@users.noreply.github.com>
2026-01-06 20:47:55 +0100

    mid

---
+++ src/bin/FizzBuzz.rs
@@ -0,0 +1,41 @@
+use std::fmt::format;
+
+struct Solution;
+
+
+fn main() {
+    let mut cases = vec![];
+
+    cases.push(Solution::fizz_buzz(3));
+    cases.push(Solution::fizz_buzz(5));
+    cases.push(Solution::fizz_buzz(15));
+
+    println!("{:?}", cases)
+}
+
+
+//IMPORTANT!! Submit Code Region Begin(Do not remove this line)
+impl Solution {
+    pub fn fizz_buzz(n: i32) -> Vec<String> {
+        let mut res = vec![];
+        for i in 1..n+1 {
+            let mut r : Vec<String> = vec![];
+            let fizz = i% 3 == 0;
+            let buzz = i % 5 == 0;
+
+            if fizz {
+                r.push("Fizz".to_string());
+            }
+            if buzz {
+                r.push("Buzz".to_string());
+            }
+
+            if !fizz && !buzz {
+                r.push(i.to_string());
+            }
+            res.push(r.join(""))
+        }
+        res
+    }
+}
+//IMPORTANT!! Submit Code Region End(Do not remove this line)

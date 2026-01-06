0f266bbfbcfc5bd8de7cdef7dd3b9799efe0faea
slyk26 <102811034+slyk26@users.noreply.github.com>
2026-01-06 16:08:29 +0100

    aced

---
+++ src/bin/NumberOfStepsToReduceANumberToZero.rs
@@ -0,0 +1,30 @@
+struct Solution;
+
+
+fn main() {
+    let mut cases = vec![];
+
+    cases.push(Solution::number_of_steps(14));
+    cases.push(Solution::number_of_steps(8));
+    cases.push(Solution::number_of_steps(123));
+
+    println!("{:?}", cases)
+}
+
+
+//IMPORTANT!! Submit Code Region Begin(Do not remove this line)
+impl Solution {
+    pub fn number_of_steps(mut num: i32) -> i32 {
+        let mut steps = 0;
+        while num > 0 {
+            if num % 2 == 0 {
+                num /= 2;
+            } else {
+                num -= 1;
+            }
+            steps += 1;
+        }
+        steps
+    }
+}
+//IMPORTANT!! Submit Code Region End(Do not remove this line)

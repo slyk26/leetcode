dcaccc10723b6f9fe7a76a10b2eb4b44d7b000aa
slyk26 <102811034+slyk26@users.noreply.github.com>
2026-01-06 17:22:55 +0100

    I forgot about bit manip

--- src/bin/NumberOfStepsToReduceANumberToZero.rs
+++ src/bin/NumberOfStepsToReduceANumberToZero.rs
@@ -17,8 +17,8 @@ impl Solution {
     pub fn number_of_steps(mut num: i32) -> i32 {
         let mut steps = 0;
         while num > 0 {
-            if num % 2 == 0 {
-                num /= 2;
+            if num & 1 == 0 {
+                num >>=1;
             } else {
                 num -= 1;
             }

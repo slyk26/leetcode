027152df9224669fecab648a41f7de093339729a
slyk26 <102811034+slyk26@users.noreply.github.com>
2026-01-06 22:40:55 +0100

    backup

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-for reference: https://leetcode.com/u/truckdriver5000/
+me: https://leetcode.com/u/truckdriver5000/
+
+with plugin: https://plugins.jetbrains.com/plugin/26395-leetcode-epiphany (china number1)
+
+in rustrover
+
+doing leetcodes as a job haver (lol)
--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,29 @@
+
+/// filename backup
+// #if($challenge.slug)$!velocity.camelCaseName($challenge.slug)#elseif($challenge.name)    $!velocity.camelCaseName($challenge.name)#elseif($challenge.title)    $!velocity.camelCaseName($challenge.title)#else    "Unknown"#end
+
+/// plugin backup template
+
+/*
+struct Solution;
+
+#set($rustCode = ${challenge.code})
+#set($pubFnIndex = $rustCode.indexOf("pub fn "))
+#set($afterPubFn = $rustCode.substring($pubFnIndex + 7))
+#set($parenIndex = $afterPubFn.indexOf("("))
+#set($methodName = $afterPubFn.substring(0, $parenIndex).trim())
+
+fn main() {
+    let mut cases = vec![];
+
+    cases.push(Solution::$methodName());
+    cases.push(Solution::$methodName());
+    cases.push(Solution::$methodName());
+
+    println!("{:?}", cases)
+}
+
+${challenge.code}
+ */
+
 fn main() {}

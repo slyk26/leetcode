8845fc25af73d5be2b6f5df1bc857161a5516b9c
slyk26 <102811034+slyk26@users.noreply.github.com>
2026-01-11 17:32:00 +0100

    LOL!

---
+++ src/bin/NumberOf1Bits.rs
@@ -0,0 +1,21 @@
+struct Solution;
+
+
+fn main() {
+    let mut cases = vec![];
+
+    cases.push(Solution::hamming_weight(11));
+    cases.push(Solution::hamming_weight(128));
+    cases.push(Solution::hamming_weight(2147483645));
+
+    println!("{:?}", cases)
+}
+
+
+//IMPORTANT!! Submit Code Region Begin(Do not remove this line)
+impl Solution {
+    pub fn hamming_weight(n: i32) -> i32 {
+        n.count_ones() as i32
+    }
+}
+//IMPORTANT!! Submit Code Region End(Do not remove this line)
